47dc85537de7baeb808c0c676184411732e1aa47
Oriol Ferrer Mesia <oriol@uri.cat>
2020-12-02 12:20:16 +0100

    ci fixes

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -6,7 +6,7 @@ echo "Executing ci/linux/install.sh"
 export OF_ROOT=~/openFrameworks
 export OF_ADDONS=$OF_ROOT/addons
 
-ADDONS="local-projects/ofxInterface"
+ADDONS="armadillu/ofxInterface"
 
 cd $OF_ADDONS
 
